bc9373c90086e82be384883e2eccd3540ad2c7f4
Félix Giffard <technonerdzfg@gmail.com>
2017-10-30 14:39:22 -0400

    Update passwordchecker.js

--- passwordchecker.js
+++ passwordchecker.js
@@ -1,1 +1,37 @@
 //Script to check for pwned passwords on your own website
+
+//This is a Work in progress
+
+var passwordInput = document.getElementById("passwordbox");
+var passwordErrorText = document.getElementById("verifytext");
+var passwordplain = '';
+var lastpasschecked = '';
+window.setInterval(function(){
+  passwordplain = passwordInput.value;
+  if (lastpasschecked !== passwordplain) {
+
+    if (passwordplain !== '') {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          passwordErrorText.innerHTML = 'Oh no! This password was found in a database of compromised passwords!';
+        }else {
+          if (this.status == 404) {
+            //passwordErrorText.innerHTML = 'Good news, this password has never been breached!';
+          }
+        }
+      };
+      xhttp.open('GET', 'https://haveibeenpwned.com/api/v2/pwnedpassword/' + encodeURIComponent(passwordplain));
+      xhttp.send();
+    }
+    lastpasschecked = passwordplain;
+  }
+
+}, 6000);
+
+function passwordmodified() {
+  var modifiedpassword = passwordInput.value;
+  if (modifiedpassword !== passwordplain) {
+    passwordErrorText.innerHTML = 'We are checking if your password has ever been compromised...';
+  }
+}
